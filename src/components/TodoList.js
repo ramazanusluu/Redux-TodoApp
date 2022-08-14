8350cc613811c1e498a784962924d206044a0575
Ramazan USLU <ruslu1720@gmail.com>
2022-08-14 23:39:01 +0300

    Filtreleme islemi

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-
 import { toggle, destroy } from "../redux/todos/todosSlice";
+
+let filtered = [];
 
 function TodoList() {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.todos.items);
+  const activeFilter = useSelector((state) => state.todos.activeFilter);
 
   const handleDestroy = (id) => {
     if (window.confirm("Are you sure?")) {
       dispatch(destroy(id));
     }
   };
+
+  filtered = items;
+  if (activeFilter !== "all") {
+    filtered = items.filter((todo) =>
+      activeFilter === "active"
+        ? todo.completed === false
+        : todo.completed === true
+    );
+  }
 
   return (
     <>
@@ -24,7 +35,7 @@ function TodoList() {
           </div>
         </li> */}
 
-        {items.map((item) => (
+        {filtered.map((item) => (
           <li key={item.id} className={item.completed ? "completed" : ""}>
             <div className="view">
               <input
