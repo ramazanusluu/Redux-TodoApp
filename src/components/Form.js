1efabb27516d916c94221fb7271ee4826e99fc73
Ramazan USLU <ruslu1720@gmail.com>
2022-08-14 19:14:37 +0300

    Genel gorunumun hazirlanmasi

---
+++ src/components/Form.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Form() {
+  return (
+    <>
+      <form>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+        />
+      </form>
+    </>
+  );
+}
+
+export default Form;
